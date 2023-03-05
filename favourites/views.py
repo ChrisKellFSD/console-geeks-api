983fdfa6219ee48ad7e6c592c139a50905806f15
ChrisKellFSD <chriskellcsn@gmail.com>
2023-03-05 18:09:05 +0000

    Added Like and Favourite List and Detail View

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from console_geeks_api.permissions import IsOwnerOrReadOnly
+from favourites.models import Favourite
+from favourites.serializers import FavouriteSerializer
 
-# Create your views here.
+
+class FavouriteList(generics.ListCreateAPIView):
+    """
+    List Favourites or create a Favourite if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FavouriteDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a favourite or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
